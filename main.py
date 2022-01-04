dd0ea29ee61c84cbd6e02710991f1171c7643c75
Gaw1521 <gaw1521@jagmail.southalabama.edu>
2022-01-04 10:20:25 -0600

    PC Update 2

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Dental Shadowing List
 
-# Update Date: 12/22/2021
+# Update Date: 01/04/2022
 
 
 import xlsxwriter
@@ -26,7 +26,7 @@ Dr = ["Dr. Gerald G. White, DDS", "Dr. Gerald G. White, DDS", "Dr. Gerald G. Whi
       "Dr. Shaun Small, DMD", "Dr. Shaun Small, DMD",  "Dr. Shaun Small, DMD",
       "Dr. Shaun Small, DMD", "Dr. Shaun Small, DMD", "Dr. Gerald G. White",
       "Dr. Gerald G. White", "Dr. Forrest Crabtree, DMD", "Dr. Forrest Crabtree, DMD",
-      "Dr. Forrest Crabtree, DMD", "Dr. Forrest Crabtree, DMD",
+      "Dr. Forrest Crabtree, DMD", "Dr. Forrest Crabtree, DMD", "Dr. Forrest Crabtree, DMD",
 
 
       ]
@@ -46,7 +46,7 @@ GD_Specialty = ["General Dentist", "General Dentist", "General Dentist",
                 "General Dentist", "General Dentist",  "General Dentist",
                 "General Dentist", "General Dentist", "General Dentist",
                 "General Dentist", "General Dentist", "General Dentist",
-                "General Dentist", "General Dentist",
+                "General Dentist", "General Dentist", "General Dentist",
 
                 ]
 
@@ -92,6 +92,7 @@ Date = ["08-31-2020",
         "05-19-2021",
         "12-20-2021",
         "12-23-2021",
+        "12-27-2021",
         "01-06-2022",
         "01-07-2022",
 
@@ -137,6 +138,7 @@ Hours = ["09:30",
          "3:00",
          "3:00",
          "3:00",
+         "5:00",
          "5:00",
          "5:00",
          "5:00",
